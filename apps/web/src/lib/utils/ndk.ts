ee87b455e92a515ba49c2f67d14eddcabf1a9b4b
verbiricha <bandarra+gith@protonmail.com>
2024-04-20 21:09:46 +0200

    fix relay URL

--- apps/web/src/lib/utils/ndk.ts
+++ apps/web/src/lib/utils/ndk.ts
@@ -55,7 +55,7 @@ export async function configureDefaultNDK(nodeFetch: typeof fetch) {
 	$ndk.addExplicitRelay('wss://nos.lol', undefined, false);
 	$ndk.addExplicitRelay('wss://relay.noswhere.com', undefined, false);
 	$ndk.addExplicitRelay('wss://relay.primal.net', undefined, false);
-	$ndk.addExplicitRelay('wss://relay.damus.com', undefined, false);
+	$ndk.addExplicitRelay('wss://relay.damus.io', undefined, false);
 	$ndk.addExplicitRelay('wss://njump.me', undefined, false);
 	$ndk.addExplicitRelay('wss://relay.nostr.band', undefined, false);
 
